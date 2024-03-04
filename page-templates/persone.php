2e258fab00595249b2684315561b4349ab726580
TeneMax <162067909+TeneMax@users.noreply.github.com>
2024-03-04 01:25:44 +0100

    Add display_users_by_role function for sorted user display
    This commit introduces the `display_users_by_role` function, addressing two primary needs: displaying users by their specific roles and sorting them by last name in ascending order. Prior to this change, users were not sorted by last name, and the code to display users for different roles contained redundancy.
    
    Changes include:
    
    1. The `display_users_by_role` function dynamically constructs query arguments to fetch users by role and sort them by their last names. This reduces code duplication and centralizes user display logic.
    
    2. The function updates the global variable `$autore` with the details of the current user in the loop before calling `get_template_part("template-parts/autore/card-persona")`, ensuring that the correct user information is displayed in each instance.
    
    3. Sorting by last name is achieved by adjusting the query arguments (`$args`), setting 'meta_key' to 'last_name', 'orderby' to 'meta_value', and 'order' to 'ASC'. This sorts users within each role category in ascending order by their last names.
    
    This improvement simplifies the codebase by removing redundancy and enhances the user interface by ensuring users are listed in a logical order. It's a straightforward update aimed at improving data presentation and site maintainability.
    
    Testing confirmed that the function works as intended, accurately displaying and sorting users by role and last name.
    
    This change is expected to facilitate easier management and display of user data, particularly in environments with a diverse and extensive user base.

--- page-templates/persone.php
+++ page-templates/persone.php
@@ -8,79 +8,54 @@
 global $post;
 get_header();
 
+function display_users_by_role($role, $title) {
+    $args = array(
+        'meta_key' => 'last_name',
+        'orderby' => 'meta_value',
+        'order' => 'ASC',
+        'meta_query' => array(
+            'relation' => 'AND',
+            array(
+                'key' => '_dsi_persona_ruolo_scuola',
+                'value' => $role,
+                'compare' => '='
+            )
+        )
+    );
+    $users = get_users($args);
+    $number_of_users = count($users);
+    if ($number_of_users > 0) {
+        ?>
+        <section class="section mt-4">
+            <div class="container">
+                <div class="title-section mb-5">
+                <h2 class="h4"><?php echo $title; ?></h2>
+                </div>
+            <div class="row variable-gutters">
+            <?php
+                foreach ($users as $user) {
+                    global $autore; // Assicura che sia globale
+                    $autore = get_user_by("ID", $user->ID); // Aggiorna con i dettagli dell'utente corrente
+                    get_template_part("template-parts/autore/card-persona");
+                }
+            ?>
+            </div>      
+        </section>  
+        <?php
+    }
+}
+
 ?>
+
 <main id="main-container" class="main-container redbrown">
-	
-	<?php get_template_part("template-parts/common/breadcrumb"); ?>
-	
-	<?php get_template_part("template-parts/hero/persone"); ?>
+    
+    <?php get_template_part("template-parts/common/breadcrumb"); ?>
+    
+    <?php get_template_part("template-parts/hero/persone"); ?>
 
-<?php 
-$args = array('meta_key' => '_dsi_persona_ruolo_scuola','meta_value'	=>	'dirigente'); 			
-$users = get_users($args);
-$number_of_users = count($users); 
-if ($number_of_users > 0) {
-?>	
-	<section class="section mt-4">
-		<div class="container">
-			<div class="title-section mb-5">
-			<h2 class="h4">Dirigente Scolastico</h2>
-			</div>	
-		<div class="row variable-gutters">	
-		<?php  
-			foreach($users as $user){
-			$autore = get_user_by("ID", $user->data->ID);
-			get_template_part("template-parts/autore/card-persona");
-			}
-		?>
-		</div>		
-	</section>					
-<?php } ?>
-
-<?php 
-$args = array('meta_key' => '_dsi_persona_ruolo_scuola','meta_value'	=>	'docente'); 			
-$users = get_users($args);
-$number_of_users = count($users); 
-if ($number_of_users > 0) { 
-?>				
-	<section class="section mt-4">
-		<div class="container">
-			<div class="title-section mb-5">
-			<h2 class="h4">Personale docente</h2>
-			</div>		
-		<div class="row variable-gutters">	
-		<?php  
-			foreach($users as $user){
-			$autore = get_user_by("ID", $user->data->ID);
-			get_template_part("template-parts/autore/card-persona");
-			}
-		?>
-		</div>		
-	</section>	
-<?php } ?>
-
-
-<?php 
-$args = array('meta_key' => '_dsi_persona_ruolo_scuola','meta_value'	=>	'personaleata'); 			
-$users = get_users($args);
-$number_of_users = count($users); 
-if ($number_of_users > 0) {
-?>
-	<section class="section mt-4">
-		<div class="container">
-			<div class="title-section mb-5">
-			<h2 class="h4">Personale non docente</h2>
-			</div>	
-		<div class="row variable-gutters">	
-		<?php  
-			foreach($users as $user){
-			$autore = get_user_by("ID", $user->data->ID);
-			get_template_part("template-parts/autore/card-persona");
-			}
-		?>
-		</div>		
-	</section>	
-<?php } ?>
+    <?php display_users_by_role('dirigente', 'Dirigente Scolastico'); ?>
+    <?php display_users_by_role('docente', 'Personale docente'); ?>
+    <?php display_users_by_role('personaleata', 'Personale non docente'); ?>
 
 </main>
 
